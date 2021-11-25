0104b48cdf4b709f5acf935726ae803f44464b9a
Brex52 <84373736+Brex52@users.noreply.github.com>
2021-11-25 09:03:31 +0100

    ho aggiunto una cartella

---
+++ iot-protocols-master/client/Client/Sensors/VirtualPositionSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Sensors
+{
+    class VirtualPositionSensor : SensorInterface, PositionSensorInterface
+    {
+        public string toJson()
+        {
+            return "{\"positionX\": " + GetPositionX() + "}" + "{\"positionX\": " + GetPositionY() + "}";
+        }
+
+        public int GetPositionX()
+        {
+            var randomX = new Random();
+            return randomX.Next(1000);
+        }
+        public int GetPositionY()
+        {
+            var randomY = new Random();
+            return randomY.Next(1000);
+        }
+
+    }
+}
